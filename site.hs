fd8ab25cd2cd7b877207c2252cd4e841e4743dc9
bendo <zdeno.osina@gmail.com>
2017-08-23 00:44:50 +0200

    Refactor number of shown posts

--- site.hs
+++ site.hs
@@ -76,9 +76,9 @@ main = hakyll $ do
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- fmap (take 4) . recentFirst =<< loadAll "posts/*"
             let indexCtx =
-                    listField "posts" postCtx (return $ take 4 posts) `mappend`
+                    listField "posts" postCtx (return posts)          `mappend`
                     constField "title" "Home"                         `mappend`
                     defaultContext
 
